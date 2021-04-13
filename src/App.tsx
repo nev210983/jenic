e4117acbade65cc7de414b2e47817078c2554c4f
Engheni Nujnenco <nev30inbox@gmail.com>
2021-04-13 20:48:27 +0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.scss';
+import Panel from './components/header/Panel'
+
+function App() {
+  return (
+      <div className="container">
+        <Panel/>
+        {/* <NavBar/> */}
+        {/* <div className="">header</div>
+        <div className="main">main</div> */}
+      </div>
+    );
+}
+
+export default App;
---
+++ src/components/main/Main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.scss'
+
+import Lists from './lists/Lists'
+
+function Main() {
+        return (
+            <div className="main">
+                <Lists />
+                <Lists />
+                <Lists />
+               
+            </div>
+        );
+    }
+
+export default Main;                
